e602557295026b31cedd8fc35840685889473ca8
Pulkit <shahpulkit@gmail.com>
2026-05-21 08:35:31 +0530

    Fix new code

--- resumeApp/views.py
+++ resumeApp/views.py
@@ -225,7 +225,7 @@ class Register(APIView):
             password=password,
             email=email
         )
-        send_welcome_email(user)
+        # send_welcome_email(user)
         return Response({"message": "Register successfully"}, status=201)
 @method_decorator(ratelimit(key='ip', rate='5/m', block=True), name='post')        
 class Login(APIView):
